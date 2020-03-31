e11026211443a9a105280660a1835276633f79cc
Luís Carlos Teixeira <luislclt@users.noreply.github.com>
2020-03-31 20:49:18 +0100

    Update install_metallb.sh
    Currente version

--- 1-MetalLB/install_metallb.sh
+++ 1-MetalLB/install_metallb.sh
@@ -1,2 +1,2 @@
-kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.7.3/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.3/manifests/metallb.yaml
 
