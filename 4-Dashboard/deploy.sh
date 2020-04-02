4d1e2f772e0db98d57b3f25f7ef2b2c289246ea5
Luís Carlos Teixeira <luislclt@users.noreply.github.com>
2020-04-02 23:04:55 +0100

    Update deploy.sh

--- 4-Dashboard/deploy.sh
+++ 4-Dashboard/deploy.sh
@@ -3,3 +3,5 @@
 kubectl apply -f dashboard-admin-account.yaml
 kubectl apply -f dashboard.yaml
 kubectl apply -f dashboard-ingress.yaml
+
+#kubectl patch deployment kubernetes-dashboard -n kube-system — patch '{"spec": {"template": {"spec": {"nodeSelector": {"beta.kubernetes.io/arch": "arm64"}}}}}'
