ba1ecf6822f55f9852d11750edd9a67b422d8204
Luís Carlos Teixeira <luislclt@users.noreply.github.com>
2020-04-02 22:47:20 +0100

    Update and rename deploy to deploy.sh

--- 3-NFS_Storage/deploy.sh
+++ 3-NFS_Storage/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-kubectl apply -f .
+
+kubectl apply -f 1-namespace.yaml
+kubectl apply -f 2-rbac.yaml
+kubectl apply -f 3-deployment-arm.yaml
+kubectl apply -f 4-storageclass.yaml
+
 kubectl patch storageclass nfs-ssd1 -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
 kubectl patch deployment nfs-client-provisioner -n nfs-storage --patch '{"spec": {"template": {"spec": {"nodeSelector": {"beta.kubernetes.io/arch": "arm64"}}}}}'
