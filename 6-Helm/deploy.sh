4d137f80d889107c809a3cb7555abd4c9e1774aa
Luís Carlos Teixeira <luislclt@users.noreply.github.com>
2020-04-03 01:16:45 +0100

    Update deploy.sh

--- 6-Helm/deploy.sh
+++ 6-Helm/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 kubectl create -f rbac-config.yaml
-helm init --service-account tiller --tiller-image jessestuart/tiller
+
+#helm init --service-account tiller --tiller-image jessestuart/tiller
+$ helm init --tiller-image=jessestuart/tiller:v2.9.0 --service-account tiller
 
 # Patch Helm to land on an ARM node because of the used image
 #kubectl patch deployment tiller-deploy -n kube-system --patch '{"spec": {"template": {"spec": {"nodeSelector": {"beta.kubernetes.io/arch": "arm64"}}}}}'
